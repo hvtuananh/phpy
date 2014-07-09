dbb19ea27b9fd41f9c981c5978bd5c71b624e854
Tuan Anh Hoang Vu <tuananh@nyu.edu>
2014-07-09 16:20:08 -0400

    fixed problem with explode and negative limit

--- string.py
+++ string.py
@@ -1,10 +1,12 @@
 def implode(glue, pieces):
     return str(glue).join(map(str, pieces))
 
-#TODO: Negative limit is not yet working
 def explode(delimiter, string, limit = None):
     if limit is None:
         return string.split(delimiter)
     else:
-        return string.split(delimiter, limit)
+        if limit < 0:
+            return string.split(delimiter, limit)[:limit]
+        else:
+            return string.split(delimiter, limit)
             
--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from string import *
 print implode(',', [1,2,3,4])
 print explode(',', "1,2,3,4")
 print explode(',', "1,2,3,4", 2)
+print explode(',', "1,2,3,4", 0)
 print explode(',', "1,2,3,4", -1)
