a6cfe1bb47b5725db1eec95fa8894d6d99c0d243
tuananh <tuananh@nyu.edu>
2014-07-10 14:05:00 -0400

    added trim functions

--- string.py
+++ string.py
@@ -13,5 +13,14 @@ def implode(glue, pieces):
 def join(glue, pieces):
     return implode(glue, pieces)
     
+def ltrim(string, character_mask = " \t\n\r\0\x0B"):
+    return string.lstrip(character_mask)
+
+def rtrim(string, character_mask = " \t\n\r\0\x0B"):
+    return string.rstrip(character_mask)
+        
 def strlen(string):
     return len(string)
+    
+def trim(string, character_mask = " \t\n\r\0\x0B"):
+    return string.strip(character_mask)
